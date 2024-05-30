f3afa6ca9c134563702181ae9bfee80eb2f78efb
a19hu <a19hu20osh1jkl@gmail.com>
2024-05-30 18:01:04 +0530

    bug/cors

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ app.use(cors(
         credentials: true
     }
 ));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.json());
